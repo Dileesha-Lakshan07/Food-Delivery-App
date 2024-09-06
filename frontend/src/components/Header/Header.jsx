47fdb09081d1986dc3a26f47cffdcbc9756d4d71
DILEESHA\dlaks <dlakshansamaraweera@gmail.com>
2024-09-06 21:16:00 +0530

    create a header part.

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Food Here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aliquid dignissimos obcaecati quam facilis voluptatem vitae, quidem magni hic quos nihil, perspiciatis repellat! Aperiam sunt perspiciatis beatae velit quasi aliquid?</p>
+
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
